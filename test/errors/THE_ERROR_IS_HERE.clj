df85df4a6cad8645896a501d855a6a20f646b77f
Aaron Lemmon <lemmo031@morris.umn.edu>
2014-11-13 12:29:04 -0600

    Did some more testing on the errors in THE_ERROR_IS_HERE file to help find information on the errors

--- test/errors/THE_ERROR_IS_HERE.clj
+++ test/errors/THE_ERROR_IS_HERE.clj
@@ -1,4 +1,7 @@
-(ns errors.THE_ERROR_IS_HERE)
+(ns errors.THE_ERROR_IS_HERE
+  (:require
+   [corefns.corefns :refer :all]
+   ))
 
 ;; (filter-stacktrace (:trace-elems (stacktrace/parse-exception (run-and-catch-raw '(a)))))
 (defn a []
@@ -7,3 +10,10 @@
 ;(:trace-elems (stacktrace/parse-exception (run-and-catch-raw '(b))))
 (defn b []
   (int "banana"))
+
+;(filter-stacktrace (:trace-elems (stacktrace/parse-exception (run-and-catch-raw '(c)))))
+(defn c []
+  (doall (map :not-a-function [1 2 3])))
+
+(defn d []
+  (hash-map "c" :d "d"))
