05fb3a547203bfc0f11be5883947c8f0007c1ea7
Henry Fellows <fello056@morris.umn.edu>
2015-05-19 11:34:14 -0500

    Moving the compilation errors

---
+++ exceptions/compilation_errors/unable_to_resolve_symbol_hello.clj
@@ -0,0 +1,4 @@
+(ns compilation_errors.unable_to_resolve_symbol_hello)
+
+;(print Hello World)
+print(Hello World)
---
+++ exceptions/compilation_errors/unable_to_resolve_world.clj
@@ -0,0 +1,3 @@
+(ns compilation_errors.unable_to_resolve_symbol_world)
+
+;(print 'Hello World')
