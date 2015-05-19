835594d4488bcaec2ddae3e85dde9fb51a016fc2
elenam <elenam@morris.umn.edu>
2015-05-18 21:10:41 -0500

    added the new test file

---
+++ src/intro/may2015.clj
@@ -0,0 +1,8 @@
+(ns intro.may2015
+  (:require [expectations :refer :all]
+            ;[corefns.corefns :refer :all]
+            [corefns.collection_fns :refer :all]))
+
+;(defn 4 5)
+(+ 4 5)
+(+ 4 5 "apple")
