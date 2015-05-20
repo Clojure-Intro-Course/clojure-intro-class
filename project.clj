4cddcd7a9301ffb32351c65a8fba71974e432a15
Henry Fellows <fello056@morris.umn.edu>
2015-05-20 12:21:38 -0500

    Updated to latest plugins, bumped version number

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject intro "0.2.2"
   :description "A pilot project to use Clojure for introductory computer science courses at the University of Minnesota - Morris"
   :dependencies [
-                 ;[org.clojure/clojure "1.7.0-beta3"] ;;Required by libraries.
-                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojure "1.7.0-beta3"] ;;Required by libraries.
                  [clj-stacktrace "0.2.8"] ;;No updates for over a year
                  [org.clojure/core.incubator "0.1.2"] ;;No updates for over a year
-                 [seesaw "1.4.3"]
-                 [expectations "2.0.9"]]
-  :plugins [[lein-autoexpect "1.0"]
+                 [seesaw "1.4.5"]
+                 [expectations "2.1.1"]]
+  :plugins [[lein-autoexpect "1.4.2"]
             [lein-pprint "1.1.2"]]
   :main intro.core)
