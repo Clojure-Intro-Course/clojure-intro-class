550a7191709d4303a1bbe5260e725251359ee9a6
Henry Fellows <fello056@morris.umn.edu>
2015-05-22 10:00:57 -0500

    Fixed an accidental deletion

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject intro "0.2.5"
+  :description "A pilot project to use Clojure for introductory computer science courses at the University of Minnesota - Morris"
+  :dependencies [
+                 [org.clojure/clojure "1.7.0-beta3"]
+                 [clj-stacktrace "0.2.8"] ;;No updates for over a year
+                 [org.clojure/core.incubator "0.1.2"] ;;No updates for over a year
+                 [seesaw "1.4.5"]
+                 [expectations "2.1.1"]]
+  :plugins [[lein-autoexpect "1.4.3"]
+            [lein-pprint "1.1.2"]]
+  :main intro.core)
