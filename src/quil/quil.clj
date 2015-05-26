bef2c7c5dd4ffe761fb86da6b0a404ff2263e6f2
Rorschach22 <mcart046@morris.umn.edu>
2015-05-26 09:35:17 -0500

    fixed error with quil.clj

--- src/quil/quil.clj
+++ src/quil/quil.clj
@@ -7,9 +7,9 @@
 
 (def opaque 255)
 
-;(def color-codes
-;     {:red [255 0 0] :blue [0 0 255] :yellow [255 255 0] :green [0 128 0] :purple [128 0 128] :orange [255 165 0] :pink [255 192 203] :black [0 0 0] :brown [165 42 42] :white [255 255 255] :grey [128 128 128] :silver [192 192 192] :gold [255 215 0] :cyan [0 255 255] :magenta [255 0 255] :maroon [128 0 0] :navy [0 0 128] :lime [0 255 0] :teal [0 128 128]}
-;)
+(def color-codes
+     {:red [255 0 0] :blue [0 0 255] :yellow [255 255 0] :green [0 128 0] :purple [128 0 128] :orange [255 165 0] :pink [255 192 203] :black [0 0 0] :brown [165 42 42] :white [255 255 255] :grey [128 128 128] :silver [192 192 192] :gold [255 215 0] :cyan [0 255 255] :magenta [255 0 255] :maroon [128 0 0] :navy [0 0 128] :lime [0 255 0] :teal [0 128 128]}
+)
 
 (def key-codes
   {:up-arrow 38, :down-arrow 40, :left-arrow 37, :right-arrow 39, :space 32, :shift 16, :tab 9, :backspace 8, :enter 10, :ctrl 17, :alt 18, :inverted-comma 192, :grave 222}
