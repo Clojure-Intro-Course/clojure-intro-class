cd0a03043b3146be11d28ce2d3a8c73e1586b37e
rabid-angel <hagen715@morris.umn.edu>
2015-06-01 10:31:38 -0500

    committing the game

--- src/intro/may2015.clj
+++ src/intro/may2015.clj
@@ -1,139 +1,200 @@
 (ns intro.may2015
-  (:require [expectations :refer :all]
+(:require [expectations :refer :all]
             [corefns.corefns :refer :all]
             [corefns.collection_fns :refer :all]
             [quil.core :as q]
             [quil.middleware :as m]
             [quil.quil :refer :all :as qq]))
 
-
-
 (defn setup []
-  "Scene setup - is called only ones at start"
-  (q/frame-rate 40)
-  (q/color-mode :rgb); cell body color green
+  "Sets up the inital state of the game.
+   From setup, you can set your framerate and add or remove variables you use."
 
-  ; initial state
-  {:board  [[0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0]
-            [0 0 0 0 0 0 0]]
-   :active-player 1
-   :player-win false})
+  (q/frame-rate 30)
+  (q/color-mode :rgb)
+  ;Put variables and their starting values for your game in the hash-map after this comment.
+  {:speed 1
+   :level 1
+   :box-1-points 0
+   :box-2-points 0
+   :box-1-pos {:x 0 :y (- (q/height) 50)}
+   :box-2-pos {:x (- (q/width) 50) :y (- (q/height) 50)}
+   :rocks []
+   :hit-player 0})
 
-(defn draw-square [x y r g b c]
-  (q/fill r g b)
-  (q/rect (* x 100) (* y 100) 100 100 c))
+(defn update-speed [state] (:speed state))
+(defn update-level[state] (:level state))
+(defn update-box-1-points [state] (:box-1-points state))
+(defn update-box-2-points [state] (:box-2-points state))
 
-(defn draw-board [state]
-  (doseq [y (range 6)
-          x (range 7)]
-    (cond (= 0 (get (get (:board state) y) x))
-          (draw-square x y 80 255 80 0)
-          (= 1 (get (get (:board state) y) x))
-          (draw-square x y 255 0 0 100)
-          (= -1 (get (get (:board state) y) x))
-          (draw-square x y 10 10 10 100)
-          :else (println "You broke something!"))))
-
-(defn bn [state x y]
-  (get (get (:board state) y) x))
-
-(defn update-board [state x]
-  (if
-    (not= (get (get (:board state) 0) x) 0)
-    state
-    (doseq [y (range 6)]
-      (if
-        (= (bn state x y) 0)
-        (def state-new (assoc state
-                         :board (assoc (:board state) y (assoc (get (:board state) y) x (:active-player state)))
-                         :active-player (- (:active-player state))))
-        (:board state))))
-    state-new)
-
-(defn draw-red-board [state]
-  (q/fill 0)
-  (q/text "Red Wins" 320 300)
-  (q/text "Right Click to Reset" 300 500))
-
-(defn draw-black-board [state]
-  (q/fill 0)
-  (q/text "Black Wins" 320 300)
-  (q/text "Right Click to Reset" 300 500))
+(defn spawn-rocks? [state] (= (quot (rand-int 6) 1) 5))
 
 
-(defn reset-board [state]
-  (if (= (:player-win state) true)
-  (assoc state :player-win false :board [[0 0 0 0 0 0 0]
-                                         [0 0 0 0 0 0 0]
-                                         [0 0 0 0 0 0 0]
-                                         [0 0 0 0 0 0 0]
-                                         [0 0 0 0 0 0 0]
-                                         [0 0 0 0 0 0 0]])
-    state))
-
-(defn check-win [state]
-  (def state-new state)
-  (doseq [y (range 6)
-          x (range 4)]
-    (def z (+ (bn state x y) (bn state (+ x 1) y) (bn state (+ x 2) y) (bn state (+ x 3) y)))
-    (if (or (= 4 z) (= -4 z)) (def state-new (assoc state-new :player-win true))))
-  (doseq [y (range 3)
-          x (range 7)]
-    (def z (+ (bn state x y) (bn state x (+ y 1)) (bn state x (+ y 2)) (bn state x (+ y 3))))
-    (if (or (= 4 z) (= -4 z)) (def state-new (assoc state-new :player-win true))))
-  (doseq [y (range 3)
-          x (range 4)]
-    (def z (+ (bn state x y) (bn state (+ x 1) (+ y 1)) (bn state (+ x 2) (+ y 2)) (bn state (+ x 3) (+ y 3))))
-    (if (or (= 4 z) (= -4 z)) (def state-new (assoc state-new :player-win true))))
-  (doseq [y (range 3)
-          x (range 3 7)]
-    (def z (+ (bn state x y) (bn state (- x 1) (+ y 1)) (bn state (- x 2) (+ y 2)) (bn state (- x 3) (+ y 3))))
-    (if (or (= 4 z) (= -4 z)) (def state-new (assoc state-new :player-win true))))
-  (if (empty? (for [y (range 6)
-                         x (range 7)
-                         :when (= (bn state x y) 0)]
-                         1)) (def state-new (reset-board (assoc state :player-win true))))
-  state-new)
-
-(defn player-won [state]
-  (if (= (:active-player state) -1) (draw-red-board state) (draw-black-board state)))
-
-
-(defn update [state]
-  state)
-
-(defn draw [state]
-  "Draws current state"
-  (q/background 80 255 80)
-    (q/stroke 255 255 255)
-  (if (not (:player-win state))
-    (draw-board state) (player-won state)))
+(defn move-rocks [state]
+  (vec (for [x (:rocks state)
+             :when (> (q/height) (second x))]
+         (vector (first x) (+ 10 (second x))))))
 
 
 
-(defn mouse-clicked [state event]
-  (cond (= (:button event) :right)
-        (reset-board state)
-        (:player-win state)
-        state
-        (= (:button event) :left)
-        (check-win (update-board state (quot (:x event) 100)))
-        (= (:button event) :center)
-        state
-        :else "you broke something")
-  )
+(defn update-rocks [state]
+  (move-rocks
+   (if (spawn-rocks? state)
+    (assoc state :rocks (conj (:rocks state) [(* (quot (rand-int 16) 1) 50) 0]))
+    state)))
+
+
+(defn hit [x1 y1 x2 y2 rx ry]
+  (println "got to hit")
+  (cond (and (= rx x1) (> ry y1) (< ry (+ 50 y1)))
+        1
+        (and (= rx x2) (> ry y2) (< ry (+ 50 y2)))
+        2
+        :else
+        0))
+
+
+(defn hit-player [state]
+  (first
+  (conj (vec (for [r (:rocks state)
+                   :let [x1 (:x (:box-1-pos state))
+                         y1 (:y (:box-1-pos state))
+                         x2 (:x (:box-2-pos state))
+                         y2 (:y (:box-2-pos state))
+                         check (hit x1 y1 x2 y2 (first r) (second r))]
+                   :when (not= check 0)]
+                   check)) 0)))
+
+
+
+(defn draw-rocks [state]
+
+  (doseq [a (:rocks state)]
+  (q/fill (+ 100 (rand-int 106)) 0 0)
+  (q/rect (first a) (second a) 50 50)))
+
+
+(defn direction [dir]
+  (cond (or (= dir :w) (= dir :up))
+        [0 -50]
+        (or (= dir :s) (= dir :down))
+        [0 50]
+        (or (= dir :a) (= dir :left))
+        [-50 0]
+        (or (= dir :d) (= dir :right))
+        [50 0]
+        :else (println "You broke your keyboard!")))
+
+(defn edge-of-map1? [dir state]
+  (or (and (= (- (q/width) 50) (:x (:box-1-pos state))) (= dir [50 0]))
+      (and (= 0 (:x (:box-1-pos state))) (= dir [-50 0]))
+      (and (= (- (q/height) 50) (:y (:box-1-pos state))) (= dir [0 50]))
+      (and (= 0 (:y (:box-1-pos state))) (= dir [0 -50]))))
+
+(defn edge-of-map2? [dir state]
+  (or (and (= (- (q/width) 50) (:x (:box-2-pos state))) (= dir [50 0]))
+      (and (= 0 (:x (:box-2-pos state))) (= dir [-50 0]))
+      (and (= (- (q/height) 50) (:y (:box-2-pos state))) (= dir [0 50]))
+      (and (= 0 (:y (:box-2-pos state))) (= dir [0 -50]))))
+
+
+(defn update-box-1-pos [dir state]
+  (if (not (edge-of-map1? dir state))
+  (assoc (:box-1-pos state)
+    :x (+ (:x (:box-1-pos state)) (first dir))
+    :y (+ (:y (:box-1-pos state)) (second dir)))
+    (:box-1-pos state)))
+;For changing player speed, look here! ^^^
+
+
+(defn update-box-2-pos [dir state]
+  (if (not (edge-of-map2? dir state))
+  (assoc (:box-2-pos state)
+    :x (+ (:x (:box-2-pos state)) (first dir))
+    :y (+ (:y (:box-2-pos state)) (second dir)))
+    (:box-2-pos state)))
+;For changing player speed, look here! ^^^
+
+(defn move [state event]
+  (cond (or (= (:key event) :d) (= (:key event) :a) (= (:key event) :w) (= (:key event) :s))
+        (assoc state :box-1-pos (update-box-1-pos (direction (:key event)) state))
+
+        (or (= (:key event) :right) (= (:key event) :left) (= (:key event) :up) (= (:key event) :down))
+        (assoc state :box-2-pos (update-box-2-pos (direction (:key event)) state))
+        :else state))
+
+
+(defn draw-points [state]
+    (q/fill 255)
+    (q/text-num (:box-1-points state) 25 50)
+    (q/text-num (:box-2-points state) 750 50))
+
+
+
+(defn update-state [state]
+  "Takes in the current state and returns the updated state.
+   Put functions that change your world state here"
+  (cond (= (:hit-player state) 1)
+        ({:speed (update-speed state)
+        :level (update-level state)
+        :box-1-points (update-box-1-points state)
+        :box-2-points (update-box-2-points state)
+        :box-1-pos (:box-1-pos state)
+        :box-2-pos (:box-2-pos state)
+        :rocks (update-rocks state)
+        :hit-player (hit-player state)})
+
+        (= (:hit-player state) 2)
+        ({:speed (update-speed state)
+          :level (update-level state)
+          :box-1-points (update-box-1-points state)
+          :box-2-points (update-box-2-points state)
+          :box-1-pos (:box-1-pos state)
+          :box-2-pos (:box-2-pos state)
+          :rocks (update-rocks state)
+          :hit-player (hit-player state)})
+      :else
+        {:speed (update-speed state)
+        :level (update-level state)
+        :box-1-points (update-box-1-points state)
+        :box-2-points (update-box-2-points state)
+        :box-1-pos (:box-1-pos state)
+        :box-2-pos (:box-2-pos state)
+        :rocks (update-rocks state)
+         :hit-player (hit-player state)}))
+
+
+
+
+(defn draw-state [state]
+  (let [x1 (:x (:box-1-pos state))
+        y1 (:y (:box-1-pos state))
+        x2 (:x (:box-2-pos state))
+        y2 (:y (:box-2-pos state))]
+    (q/background 0)
+    (q/stroke 255)
+    (q/fill 80 255 80)
+    (q/rect x1 y1 50 50)
+    (if (and (= x1 x2) (= y1 y2))
+      (q/fill 60 235 148)
+    (q/fill 80 255 255))
+    (q/rect x2 y2 50 50)
+
+    (draw-rocks state)
+    (draw-points state)
+
+    (println state)))
 
 (q/defsketch gol
-  :title "Connect Four"
-  :size [700 600]
+  :title "Game"
+  :size [800 1000]
+  ; Setup function called only once, during sketch initialization.
   :setup setup
-  :update update
-  :draw draw
-  :mouse-clicked mouse-clicked
+  ; Update-state is called on each iteration before draw-state.
+  :update update-state
+  (try
+  :draw draw-state
+    (catch Exception e (str "caught exception: " (.getMessage e))))
+  :key-pressed move
   :middleware [m/fun-mode])
 
-;(q/rect (* x 100) (* y 100) 100 100)
