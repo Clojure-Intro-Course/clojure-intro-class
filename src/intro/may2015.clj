25a47516b7dc89041dee35152b5d5903200af701
rabid-angel <hagen715@morris.umn.edu>
2015-06-04 16:16:30 -0500

    re-wrote some quil functions to have assert checks. Made a beginner startup with examples and explanations

--- src/intro/may2015.clj
+++ src/intro/may2015.clj
@@ -4,93 +4,128 @@
           [corefns.collection_fns :refer :all]
           [quil.core :as q]
           [quil.middleware :as m]
-          [quil.quil :refer :all :as qq]
+          [quil.q_functions :refer :all]
           [errors.errorgui :refer :all]
           [errors.prettify_exception :refer :all]))
 
+
+;-------------------- Do not remove things above this line --------------------
+
+;A few things to note
+
+;Quil api for functions: http://quil.info/api
+
+; !!! Add 'f-' to the front of these functions:
+;background      http://quil.info/api/color/setting#background
+;ellipse         http://quil.info/api/shape/2d-primitives#ellipse
+;fill            http://quil.info/api/color/setting#fill
+;line            http://quil.info/api/shape/2d-primitives#line
+;quad            http://quil.info/api/shape/2d-primitives#quad
+;rect            http://quil.info/api/shape/2d-primitives#rect
+;stroke          http://quil.info/api/color/setting#stroke
+;stroke-weight   http://quil.info/api/shape/attributes#stroke-weight
+;text            http://quil.info/api/typography/loading-and-displaying#text
+;text-num        http://quil.info/api/typography/loading-and-displaying#text-num
+;text-size       http://quil.info/api/typography/attributes#text-size
+;triangle        http://quil.info/api/shape/2d-primitives#triangle
+
+;!!! Add a q/ in front of any other functions from the Quil API
+
+;Ex.
+;(f-background 80 255 80)
+;(q/sqrt 64)
+;
+
+;This file currently contains code to draw something already!
+;You can use this code as a reference for setting up your project
 
 (defn setup []
   "Sets up the inital state of the game.
    From setup, you can set your framerate and add or remove variables you use."
 
-
-  (q/frame-rate 1)
+  (q/frame-rate 30)
   (q/color-mode :rgb)
-   {:light 0
-    :p1-pos [0 450]
-    :p2-pos [900 450]
-    :m-nill 100
-    })
 
-(defn not-nil? [x]
-  (not (nil? x)))
 
-(defn make-nil [state]
-  (if (= (rand-int 2) 0)
-    "A"
-    100))
+  ;Put variables and their starting values for your game in the hash-map after this comment.
+  {:color 0
+   :angle 0})
+
+
+;Use smaller functions to update parts of the state
+;----------------------
+    ;Both of these helper functions take the state as an argument and both return a number
+(defn update-color [state]
+  (mod (+ (:color state) 0.07) q/TWO-PI))
+
+(defn update-angle [state]
+  (mod (+ (:angle state) 0.1) q/TWO-PI))
+;-----------------------
 
 
 (defn update-state [state]
+  "Takes in the current state and returns the updated state.
+   Put functions that change your world state here"
+  ;-------------------- Do not remove this line --------------------
   (try
-    ;(assert
-    ;  state
-    ;"Not nil is failing")
+    (assert (not-nil? state) "Your state is nil")
+  ;-------------------- Do not remove this line --------------------
 
 
-    {:light 0
-     :p1-pos [0 450]
-     :p2-pos [900 450]
-     :m-nill (make-nil state)}
+    ;Put your code for update-state here!
+
+    ;Here we are saying what the elements of the state should update to
+    ;Make sure to include all of the elements of your state
+    {:color (update-color state)
+     :angle (update-angle state)}
 
 
+
+
+    ;-------------------- Do not remove this line --------------------
     (catch Throwable e (println (.getCause e)) (display-error (prettify-exception e)))))
+    ;-------------------- Do not remove this line --------------------
 
 
 
 
 
 (defn draw-state [state]
-  (println "Start of frame" (q/frame-count) "with :m-nill being" (:m-nill state))
-  (try
-    (let [x1 (first (:p1-pos state))
-          y1 (second (:p1-pos state))
-          x2 (first (:p2-pos state))
-          y2 (second (:p2-pos state))]
-      (q/background (* 127 (+ 1 (q/sin (:light state)))))
-      (q/fill 80 255 80)
-      (q/text-size 72)
-      (q/text-align :center)
-      (q/text-num (q/frame-count) 500 850)
+  "Draws current state"
+  ;-------------------- Do not remove this line --------------------
+   (try
+  ;-------------------- Do not remove this line --------------------
 
 
-      (println (q/fill 0))
-      (println )
-      (q/fill 80 255 80)
-      (assert (not-nil? x1) "One of your inputs is nil")
-      (assert (not-nil? y1) "One of your inputs is nil")
-      (assert (not-nil? (:m-nill state)) "One of your inputs is nil")
-      (assert (not-nil? 100) "One of your inputs is nil")
-      (q/rect x1 y1 (:m-nill state) 100)
-      (q/fill 80 255 255)
-      (q/rect x2 y2 100 100)
-      (q/fill 80 255 80)
-      (q/rect 450 0 100 100))
+     ;Put your code for draw-state here!
+     (f-background 240)
+     (let [angle (:angle state)
+           x (* 150 (q/cos angle))
+           y (* 150 (q/sin angle))
+           r (* 127 (+ 1 (q/sin (:color state))))
+           b (* 127 (+ 1 (q/sin (+ 0.2 (:color state)))))
+           g (* 127 (+ 1 (q/sin (:color state))))]
+       (f-fill r g b)
+       (q/with-translation [(/ (q/width) 2)
+                            (/ (q/height) 2)]
 
-    (catch NullPointerException e (println "\n NullPointerException is:" e "\n"))
-    (catch Throwable e (println (.getCause e)) (display-error (prettify-exception e))))
-  )
+         (f-ellipse x y 100 100)))
 
 
-(q/defsketch gol
-  :title "Game"
-  :size [1000 1000]
+  ;-------------------- Do not remove this line --------------------
+     (catch Throwable e (println (.getCause e)) (display-error (prettify-exception e)))))
+  ;-------------------- Do not remove this line --------------------
+
+
+
+
+
+(q/defsketch my
+  :title "My sketch"
+  :size [500 500]
   ; Setup function called only once, during sketch initialization.
   :setup setup
   ; Update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
-  ;:key-pressed move
   :middleware [m/fun-mode])
-
-
