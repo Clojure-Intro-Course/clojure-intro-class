9bf8bac92422dc1e2a019df17ea7e8fcb6bf55ab
elenam <elenam@morris.umn.edu>
2015-06-16 13:52:01 -0500

    fixed some failing tests

--- test/errors/error_dictionary_test.clj
+++ test/errors/error_dictionary_test.clj
@@ -29,34 +29,34 @@
 
 ;; testing for :pretty-print-value
 (expect "In function +, the second argument ((0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...) (0 1 2...)...) must be a number but is a sequence."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(+ 1 (repeat (range))))))
 
 (expect "In function +, the second argument (((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...) ((...) (...) (...)...)...) must be a number but is a sequence."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(+ 1 (repeat (repeat (range)))))))
 
 (expect "In function inc, the first argument ((0 1 2...) (1 (...) (...)...) (1 2 1...) (0 1 2...) (1 (...) (...)...) (1 2 1...) (0 1 2...) (1 (...) (...)...) (1 2 1...) (0 1 2...)...) must be a number but is a sequence."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(inc (cycle [(range) [1 (repeat 1) (range) 4 5 6 7 8 9 245 4235 5423] (cycle [1 2])])))))
 
 (expect "In function +, the second argument (1 2 3 4 5 6 76 (0 1 2...) 756 354...) must be a number but is a vector."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(+ 1 [1 2 3 4 5 6 76 (range) 756 354 6 645]))))
 
 ;; testing for :illegal-argument-no-val-supplied-for-key
 (expect "No value found for key d. Every key for a hash-map must be followed by a value."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(hash-map "c" :d "d"))))
 
 ;; testing for :illegal-argument-vector-arg-to-map-conj
 (expect "Each inner vector must be a pair: a key followed by a value."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(into {} [[1 2] [3]]))))
 
 ;; testing for :illegal-argument-cannot-convert-type
 (expect "In function cons, the second argument 2 must be a sequence but is a number."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(cons 1 2))))
 
 ;; testing for :illegal-argument-even-number-of-forms
@@ -71,12 +71,12 @@
 ;; we don't want the error to happen before we run this test.
 ;; Need to check for the location as well: currently incomplete.
 (expect #"When declaring a let, you need to pass it a vector of arguments.(.*)"
-       (get-all-text (:msg-info-obj (try (load-file "exceptions/compilation_errors/let-odd-number-bindings.clj")
+       (get-text-no-location (:msg-info-obj (try (load-file "exceptions/compilation_errors/let-odd-number-bindings.clj")
                        (catch Throwable e (prettify-exception e))))))
 
 ;; testing for :illegal-argument-type-not-supported
 (expect "Function contains? does not allow a sequence as an argument."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(contains? (seq [1 3 6]) 2))))
 
 ;; Elena: this is a compilation error in clojure 1.7, so we can't test it like this
@@ -91,7 +91,7 @@
 ;        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(when-let [num1 1 num2 2] "hello"))))
 
 (expect "Cannot call nil as a function."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(nil 5))))
 
 ;##################################################
@@ -100,12 +100,12 @@
 
 ;; testing for :index-out-of-bounds-index-provided
 (expect "An index in a sequence is out of bounds. The index is: 10."
-        (get-all-text
+        (get-text-no-location
           (run-and-catch-pretty-no-stacktrace 'intro.core '(throw (new IndexOutOfBoundsException "10")))))
 
 ;; testing for :index-out-of-bounds-index-not-provided
 (expect "An index in a sequence is out of bounds or invalid."
-        (get-all-text
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(nth [0 1 2 3 4 5] 10))))
 
 ;###################################
@@ -114,21 +114,21 @@
 
 ;; testing for :arity-exception-wrong-number-of-arguments
 (expect "You cannot pass three arguments to a function even?, need one."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(even? 3 6 1))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(even? 3 6 1))))
 
 (expect "You cannot pass zero arguments to a function odd?, need one."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(odd?))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(odd?))))
 
 (expect "You cannot pass one argument to this anonymous function."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(#(+ %1 %2) 1))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(#(+ %1 %2) 1))))
 
 (expect "You cannot pass four arguments to a function user-def-fcn."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '((defn user-def-fcn [x] (+ x 1))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '((defn user-def-fcn [x] (+ x 1))
     (user-def-fcn 1 2 3 4)))))
 
 (expect "You cannot use the same key in a hash map twice, but you have duplicated the key :b."
         ;; note: let is needed in the test since the error must happen only at run time
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(let [a :b b :b] {a 1 b 2}))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(let [a :b b :b] {a 1 b 2}))))
 
 
 ;##########################################
@@ -137,11 +137,11 @@
 
 ;; testing for :null-pointer-non-existing-object-provided
 (expect "An attempt to access a non-existing object: some message (NullPointerException)."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(throw (new NullPointerException "some message")))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(throw (new NullPointerException "some message")))))
 
 ;; testing for :null-pointer-non-existing-object-not-provided
 (expect "An attempt to access a non-existing object (NullPointerException)."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(int nil))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(int nil))))
 
 ;###################################################
 ;### Testing for Unsupported Operation Exceptions###
@@ -149,7 +149,7 @@
 
 ;; testing for :unsupported-operation-wrong-type-of-argument
 (expect "Function nth does not allow a map as an argument."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(nth {:a 10 :z 4} 20))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(nth {:a 10 :z 4} 20))))
 
 ;##################################
 ;### Testing for Java Exceptions###
@@ -164,24 +164,25 @@
 ;### Testing for compilation errors ###
 ;######################################
 
-(expect "loop requires an even number of forms in binding vector."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(defn s [s] (loop [s])))))
+;;;; Elena: we might want to revisit this one
+(expect #"Parameters for loop must come in pairs, but one of them does not have a match; on line (\d+) in the file intro\.core"
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(defn s [s] (loop [s])))))
 
-(expect #"This recur is supposed to take 0 arguments, but you are passing 1." ; this is giving NO_SOURCE_PATH
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(recur (inc 1)))))
+(expect "This recur is supposed to take 0 arguments, but you are passing 1." ; this is giving NO_SOURCE_PATH
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(recur (inc 1)))))
 
-(expect #"There is an unmatched parameter in declaration of cond." ; this is giving NO_SOURCE_PATH
-        (get-all-text
+(expect "There is an unmatched parameter in declaration of cond." ; this is giving NO_SOURCE_PATH
+        (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.core '(defn my-num [x] (cond (= 1 x))))))
 
 ;; the internal representation of zero? is zero?--inliner--4238 (in this particular test), i.e. it has
 ;; an inliner part
 ;;; Why doesn't it process the message?
 (expect "You cannot pass zero arguments to a function zero?."
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(zero? 3 4 5))))
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(zero?))))
 
-(expect #"Recur can only occur as a tail call: no operations can be done after its return." ; this is giving NO_SOURCE_PATH
-        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(defn inc-nums [x] ((recur (inc x)) (loop [x x]))))))
+(expect "Recur can only occur as a tail call: no operations can be done after its return." ; this is giving NO_SOURCE_PATH
+        (get-text-no-location (run-and-catch-pretty-no-stacktrace 'intro.core '(defn inc-nums [x] ((recur (inc x)) (loop [x x]))))))
 
 (expect #"def must be followed by a name." ; this is giving NO_SOURCE_PATH
         (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(def 4 (+ 2 2)))))
