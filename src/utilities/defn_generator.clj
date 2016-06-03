46def9c83108bcc4ceaa6f30cc7b7ce2c4d329ed
sean-stockholm <stock424@morris.umn.edu>
2016-06-03 17:42:40 -0500

    #98 DIDNT FAIL, AND APPEARS FULLY FUNCTIONAL

--- src/utilities/defn_generator.clj
+++ src/utilities/defn_generator.clj
@@ -7,7 +7,7 @@
   ;;(println *ns*)
   (let [arg-types {"coll" "seqable","c1" "seqable", "c2" "seqable","c3" "seqable","colls" "seqables",
                    "n" "number",  "s" "string", "f" "function", "&" "&"}]
-    (vec (map (fn [c] (vec (map name #_ #(arg-types (name %)) c))) (into [] col)))))
+    (vec (map (fn [c] (vec (map #(arg-types (name %)) c))) (into [] col)))))
 
 
 ;; outputs a string of generated data for redefining functions with preconditions
@@ -16,7 +16,7 @@
   ;;(println *ns*)
   (let [fmeta (meta f)]
    ;; (println *ns*)
-  (str "(re-defn " #_ (:ns fmeta) "/" (:name fmeta) " " (apply str (interpose " " (clj->ourtypes (:arglists fmeta)))) ")")))
+  (str "(re-defn #'" (:ns fmeta) "/" (:name fmeta) " " (apply str (interpose " " (clj->ourtypes (:arglists fmeta)))) ")")))
 
 
 ;; string vector vector -> string
@@ -27,30 +27,38 @@
    ;; (println *ns*)
         (cond
           (empty? col2)                     s
-         ;; (re-matches #"\&" (first col1))   s
-         ;; (nil? (first col2))               (recur s (rest col1) (rest col2))
           (= "&" (first col2))              (str s "(check-if-" (first (rest col2)) "? \"" unqualified-name "\" args)\n           ")
           :else                             (recur (str s "(check-if-" (first col2) "? \"" unqualified-name "\" " (first col1) ")\n           ") (rest col1) (rest col2)))))
 
 ;; vector -> vector
-(defn make-arg-vec [arg-types]
+;(defn make-arg-vec [arg-types]
  ; (println *ns*)
-   (loop [v []
-          types arg-types]
-   ;  (println *ns*)
-        (cond
-          (= (count v) (count arg-types))  v
-          (re-matches #".*&.*" (first types))  (conj v "& args")
-          :else                            (recur (conj v (str "argument" (inc (count v)))) (rest arg-types))
-          )
-     )
-  )
+;   (loop [v []
+;          types arg-types]
+;   ;  (println *ns*)
+;        (cond
+;          (= (count v) (count arg-types))      v
+;          (re-matches #".*&.*" (first types))  (conj v "& args")
+;          :else                                (recur (conj v (str "argument" (inc (count v)))) (rest arg-types))
+;          )
+;     )
+;  )
+
+(defn make-arg-vec [args]
+  (loop [to-vec []
+         from-vec args
+         cnt 1]
+    (cond
+      (empty? from-vec) to-vec
+      (= "&" (first from-vec)) (conj (conj to-vec "&") "args")
+      :else (recur (conj to-vec (str "argument" cnt)) (rest from-vec) (inc cnt)))))
 
 ;; string vector -> string
 (defn single-re-defn [fname arg-types]
   (let [f (symbol fname)
         unqualified-name  (name f)
-        qualified-name (str #_ (:ns f) "/" unqualified-name)
+       ; yes (println "it is: " (symbol fname))
+        qualified-name (str  #_ (:ns f) "clojure.core/" unqualified-name)
         do-apply (re-matches #".*s$" (get arg-types (dec (count arg-types)))) ; do we use apply? this is where it's decided
         arg-vec  (make-arg-vec arg-types); vector of the arguments, as goes after the name of function
         arg-str (apply str (interpose " " arg-vec)) ; space separated arguments in string form, useful for printing.
@@ -67,8 +75,14 @@
 
 ;; takes a function name, and a vector of vectors of arguments
 ;; note: arguments DO NOT end in a question mark.
-(defn ree-defn [fname & arglists]
-  (str "(defn " (name (symbol fname)) "\n" (reduce #(str %1 "\n" (single-re-defn fname %2)) "" arglists) ")"))
+(defn re-defn [fname & arglists]
+  (str "(defn "
+       (:name (meta fname))
+       "\n"
+       (reduce
+         #(str %1 "\n" (single-re-defn (:name (meta fname)) %2))
+         ""
+         arglists) ")"))
 
 
 
