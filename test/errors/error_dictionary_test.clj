f3a12f6ccb6b400d58d8d57df089f473d426b38b
Shamund Gordon <gordo580@reliant.morris.umn.edu>
2016-06-30 17:00:41 -0500

    8 fails, 2 errors left

--- test/errors/error_dictionary_test.clj
+++ test/errors/error_dictionary_test.clj
@@ -83,7 +83,7 @@
 
 ;; Note: the order of elements in a hashmap is not guaranteed, so the actual elements amy be different
 
-(expect "In function +, the argument {:a 6, :k \"a\", :b 4, :c +, :l 6, :m 7} must be a number but is a map."
+(expect "In function +, the argument {:a 6, :k \"a\", :b 4, :c +, :l 6, :m 7} must be a number but is a map,\nin the function call (+ {:a 6, :k \"a\", :b 4, :c +, :l 6, :m 7})"
         (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.student '(+ {:a 6 :k "a" :b 4 :c + :l 6 :m 7}))))
 
@@ -202,7 +202,7 @@
 
 ;; testing for empty?, first, rest, next, seq, count, dissoc, and dec
 
-(expect "In function empty?, the argument :s must be a sequence but is a keyword."
+(expect "In function empty?, the argument :s must be a sequence but is a keyword,\nin the function call (empty? :s)"
         (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.student '(empty? :s))))
 
