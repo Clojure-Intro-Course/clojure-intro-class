78cb33d2819b9f9d7d282e702301b32fac520834
Shamund Gordon <gordo580@reliant.morris.umn.edu>
2016-07-01 11:48:04 -0500

    some sorting done into seq_ops

--- test/Exception_Messages/Overwritten_Fns/seq_ops.clj
+++ test/Exception_Messages/Overwritten_Fns/seq_ops.clj
@@ -7,3 +7,20 @@
             [utilities.file_IO :refer :all]
             ))
 
+;; testing for :illegal-argument-no-val-supplied-for-key
+(expect "No value found for key d. Every key for a hash-map must be followed by a value."
+        (get-text-no-location
+         (run-and-catch-pretty-no-stacktrace 'intro.student '(hash-map "c" :d "d"))))
+
+;; testing for :illegal-argument-vector-arg-to-map-conj
+(expect "Vectors added to a map must consist of two elements: a key and a value."
+        (get-text-no-location
+         (run-and-catch-pretty-no-stacktrace 'intro.student '(into {} [[1 2] [3]]))))
+
+(expect "Vectors added to a map must consist of two elements: a key and a value."
+        (get-text-no-location
+         (run-and-catch-pretty-no-stacktrace 'intro.student '(merge {:a 1 :b 2} [1 2 3] [1 2] [1 5]))))
+
+(expect "Vectors added to a map must consist of two elements: a key and a value."
+        (get-text-no-location
+         (run-and-catch-pretty-no-stacktrace 'intro.student '(conj {:a 1 :b 2} [1 2 3 4]))))
--- test/errors/error_dictionary_test.clj
+++ test/errors/error_dictionary_test.clj
@@ -97,7 +97,6 @@
         (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.student '(into {} [[1 2] [3]]))))
 
-;;sorted to here..................................
 
 (expect "Vectors added to a map must consist of two elements: a key and a value."
         (get-text-no-location
@@ -106,6 +105,8 @@
 (expect "Vectors added to a map must consist of two elements: a key and a value."
         (get-text-no-location
          (run-and-catch-pretty-no-stacktrace 'intro.student '(conj {:a 1 :b 2} [1 2 3 4]))))
+
+;;sorted to here..................................
 
 ;; testing for :illegal-argument-cannot-convert-type
 (expect "In function cons, the second argument 2 must be a sequence but is a number."
