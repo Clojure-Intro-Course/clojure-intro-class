f7f8f004fe32f3fb6d621ce8cb875355778cf44b
elenam <elenam@morris.umn.edu>
2016-07-05 16:31:00 -0500

    fixed wording of a message about 20 parameters

--- test/errors/error_dictionary_test.clj
+++ test/errors/error_dictionary_test.clj
@@ -150,7 +150,7 @@
        (get-text-no-location (:msg-info-obj (try (load-file "exceptions/compilation_errors/greater_than_20_parameters.clj")
                        (catch Throwable e (prettify-exception e))))))
 
-(expect #"A function may not take more than more than 20 parameters."
+(expect #"A function may not take more than 20 parameters."
        (get-text-no-location (:msg-info-obj (try (load-file "exceptions/compilation_errors/greater_than_20_parameters2.clj")
                        (catch Throwable e (prettify-exception e))))))
 
