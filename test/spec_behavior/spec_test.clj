a458978ab0c644fb11610cb81918012280b12bf4
Tony Song <songx823@morris.umn.edu>
2016-07-11 17:16:19 -0500

    added some tests for spec

--- test/spec_behavior/spec_test.clj
+++ test/spec_behavior/spec_test.clj
@@ -8,7 +8,10 @@
 ;###                                      ###
 ;############################################
 
-(defn hashmap-gen [f]
+(defn hashmap-gen
+  "takes a quoted function call and returns an auto-generated
+  spec hash-map when an error happens"
+  [f]
   (try (eval f)
     (catch clojure.lang.ExceptionInfo e
       (first (vals (:clojure.spec/problems (.getData e)))))))
@@ -21,11 +24,10 @@
 ;;                 :check-seqable second
 ;;                 'seqable? last)
 ;;         (:pred (hashmap-gen '(empty? [] []))))
-(expect {:pred 'length1?, :val ([] []), :via [], :in [0]} (hashmap-gen '(empty? [] [])))
+(expect {:pred 'length1?, :val '([] []), :via [:corefns.specs/length-one], :in []} (hashmap-gen '(empty? [] [])))
+(expect {:pred 'length1?, :val nil, :via [:corefns.specs/length-one], :in []} (hashmap-gen '(empty?)))
+(expect {:pred 'seqable?, :val true, :via [], :in [0]} (hashmap-gen '(empty? true)))
+(expect false (empty? [1 2]))
+(expect true (empty? nil))
 
-;; (empty? [] [])
-;; (empty?)
-;; (empty? true)
-;; (empty? [1 2])
-;; (empty? nil)
 
