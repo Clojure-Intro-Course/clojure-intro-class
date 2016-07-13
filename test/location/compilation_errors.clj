80ecf6082a567d156db7763164b109fdebfd7577
Shamund Gordon <stgordon19@gmail.com>
2016-07-13 15:02:51 -0500

    added tests to location file

--- test/location/compilation_errors.clj
+++ test/location/compilation_errors.clj
@@ -17,6 +17,14 @@
 (expect (str "There is an unmatched delimiter ).\nFound in file unmatched_delimiter.clj" (line-number-format 3 20) ".")
         (get-all-text  (:msg-info-obj (prettify-exception (import-from-file "exceptions/unmatched_delimiter.ser")))))
 
+;###########################################
+;### Testing for ClassNotFoundException ####
+;###########################################
 (expect (more-> "Name clojure.string.split is undefined." get-text-no-location
                 (re-pattern (str "(.*)Found in (.*)" (line-number-format "(\\d+)" "(\\d+)") "\\."))  get-all-text)
               (run-and-catch-pretty-no-stacktrace 'intro.core '(clojure.string.split "a b c" " ")))
+
+
+;; testing for :illegal-argument-exactly-2-forms
+(expect #"The function when-let requires exactly 2 forms in binding vector. Line (.*) in the file intro.core"
+        (get-all-text (run-and-catch-pretty-no-stacktrace 'intro.core '(when-let [num1 1 num2 2] "hello"))))
