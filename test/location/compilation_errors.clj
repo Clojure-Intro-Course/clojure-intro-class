e0f48285bc59181a44daa50c61d71139ad8297ac
Shamund Gordon <stgordon19@gmail.com>
2016-07-13 14:55:21 -0500

    added tests to location file

--- test/location/compilation_errors.clj
+++ test/location/compilation_errors.clj
@@ -6,3 +6,17 @@
             [errors.prettify_exception :refer :all]
             [utilities.file_IO :refer :all]
             ))
+
+;######################################
+;### Testing for compilation errors ###
+;######################################
+
+(expect (str "End of file, starting at line 3.\nProbably a non-closing parenthesis or bracket.\nFound in file eof.clj" (line-number-format 4 1) ".")
+        (get-all-text (:msg-info-obj (prettify-exception (import-from-file "exceptions/end_of_file.ser")))))
+
+(expect (str "There is an unmatched delimiter ).\nFound in file unmatched_delimiter.clj" (line-number-format 3 20) ".")
+        (get-all-text  (:msg-info-obj (prettify-exception (import-from-file "exceptions/unmatched_delimiter.ser")))))
+
+(expect (more-> "Name clojure.string.split is undefined." get-text-no-location
+                (re-pattern (str "(.*)Found in (.*)" (line-number-format "(\\d+)" "(\\d+)") "\\."))  get-all-text)
+              (run-and-catch-pretty-no-stacktrace 'intro.core '(clojure.string.split "a b c" " ")))
