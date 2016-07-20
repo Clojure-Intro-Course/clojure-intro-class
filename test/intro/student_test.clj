4263303c0c8df8ad829ae528343cd530e1a978d8
Tony Song <tony99019@gmail.com>
2016-07-20 15:59:41 -0500

    Add testing for exact line numbers #123

--- test/intro/student_test.clj
+++ test/intro/student_test.clj
@@ -8,6 +8,10 @@
 
 (def location-regex (re-pattern (str "(.*)\\nFound in file student.clj" (line-number-format "(\\d+)" "(\\d+)") "(.*)")))
 (def location-regex-core (re-pattern (str "(.*)\\nFound in file core.clj" (line-number-format "(\\d+)" "(\\d+)") "(.*)")))
+(defn location-regex-exact
+  "takes a line number and returns a regex pattern with the exact line number"
+  [line-n]
+  (re-pattern (str "(.*)\\nFound in file student.clj" (line-number-format line-n nil) "(.*)")))
 
 ;; showing the resulting regex:
 (expect #"(.*)\nFound in file student.clj on, or before, line (\d+)(.*)" location-regex)
@@ -16,7 +20,7 @@
 (expect (more-of x
                  java.lang.StringIndexOutOfBoundsException (:exception-class x)
                  #"Position 12 is outside of the string\.(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 60) (get-all-text (:msg-info-obj x)) ; use location-regex for generic line number testing
                  (trace-has-all-pairs? {:fn "subs" :ns "clojure.core"}) (:stacktrace x)
                  (trace-doesnt-have-pair? :ns "expectations") (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
@@ -26,7 +30,7 @@
 (expect (more-of x
                  java.lang.ClassCastException (:exception-class x)
                  #"Attempted to use a number, but a function was expected\.(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 66) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob15" :ns "intro.student"}) (:stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob15 5)))
@@ -35,7 +39,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"You cannot pass three arguments to a function cons, need two\,\nin the function call \(cons \"Hello, \" \"Dave\" \"!\"\)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 73) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob16" :ns "intro.student"}) (:stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob16 "Dave")))
@@ -44,7 +48,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"In function \+, the second argument must be a number but is nil,\nin the function call \(\+ 5 nil\)\n(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 84) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob17" :ns "intro.student"}) (:stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob17 '(1 2 3))))
@@ -53,7 +57,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"In function <, the first argument must be a number but is nil,\nin the function call \(< nil 5\)\n(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 90) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob18" :ns "intro.student"}) (:stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob18 '(3 4 5 6 7))))
@@ -62,7 +66,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"In function \+, the second argument must be a number but is nil,\nin the function call \(\+ 15 nil\)\n(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 103) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob64" :ns "intro.student"}) (:stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob64 [1 2 3 4 5])))
@@ -100,7 +104,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"In function nth, the first argument 3 must be a sequence but is a number,\nin the function call \(nth 3 \[:a :b :c :d\]\)\n(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 140) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob20" :ns "intro.student"}) (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob20 [:a :b :c :d])))
@@ -118,7 +122,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"In function cons, the second argument 5 must be a sequence but is a number,\nin the function call \(cons 5 5\)\n(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 153) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "add-five" :ns "intro.student"}) (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(add-five 5)))
@@ -127,7 +131,7 @@
 (expect (more-of x
                  clojure.lang.ExceptionInfo (:exception-class x)
                  #"In function \+, the second argument \(2 3\) must be a number but is a sequence,\nin the function call \(\+ 1 \(2 3\)\)\n(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 160) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob24" :ns "intro.student"}) (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob24 [1 2 3])))
@@ -136,7 +140,7 @@
 (expect (more-of x
                  java.lang.IndexOutOfBoundsException (:exception-class x)
                  #"An index in a sequence is out of bounds or invalid\.(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 171) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob23" :ns "intro.student"}) (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob23 [5 4 3 2 1])))
@@ -145,7 +149,7 @@
 (expect (more-of x
                  clojure.lang.ArityException (:exception-class x)
                  #"You cannot pass zero arguments to a function equal-to\?\.(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 184) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "prob27" :ns "intro.student"}) (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob27 '(\r \a \c \e \c \a \r))))
@@ -155,7 +159,7 @@
 (expect (more-of x
                  clojure.lang.ArityException (:exception-class x)
                  #"You cannot pass one argument to this anonymous function\.(.*)" (get-all-text (:msg-info-obj x))
-                 location-regex (get-all-text (:msg-info-obj x))
+                 (location-regex-exact 194) (get-all-text (:msg-info-obj x))
                  (trace-has-all-pairs? {:fn "exercise9" :ns "intro.student"}) (:filtered-stacktrace x))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(exercise9)))
@@ -163,8 +167,8 @@
 ;; testing for prob38
 (expect (more-of x
                  clojure.lang.ArityException (:exception-class x)
-                 ;;; This is a legitimate location error, need to look into it
-                 ;location-regex (get-all-text (:msg-info-obj x)))
+                 ;;; This is a legitimate location error, need to look into it -> The line number seems to indicate this file line 174
+;;                  location-regex (get-all-text (:msg-info-obj x))
                  #"You cannot pass four arguments to a function prob38\.(.*)" (get-all-text (:msg-info-obj x)))
         (run-and-catch-pretty-with-stacktrace 'intro.student
                                               '(prob38 1 8 3 4)))
